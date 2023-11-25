c7999ec3c2ad2784715ed416b5c0ad1eb1f0c77e
Manoel Lima <manoelsslima@yahoo.com.br>
2023-11-25 01:32:12 -0400

    Using pipes

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -8,16 +8,16 @@ import { Component } from '@angular/core';
 export class LancamentosPesquisaComponent {
 
   lancamentos = [
-    { tipo : 'RECEITA', descricao: 'Venda de software', dataVencimento: '21/11/2023', dataPagamento: null, valor: 8000.00, pessoa: 'Drogaria Saúde' },
-    { tipo : 'RECEITA', descricao: 'Aluguel', dataVencimento: '05/12/2023', dataPagamento: null, valor: 1200.00, pessoa: 'Fernando Pessoa' },
-    { tipo : 'DESPESA', descricao: 'Compra de pão', dataVencimento: '19/11/2023', dataPagamento: null, valor: 6.50, pessoa: 'Padaria pão de mel' },
-    { tipo : 'DESPESA', descricao: 'Impostos', dataVencimento: '30/11/2023', dataPagamento: null, valor: 2350.00, pessoa: 'Ministério da Fazenda' },
-    { tipo : 'DESPESA', descricao: 'Mensalidade da escola', dataVencimento: '05/12/2023', dataPagamento: null, valor: 400.00, pessoa: 'Cantinho do saber' },
-    { tipo : 'DESPESA', descricao: 'Mensalidade da academia', dataVencimento: '30/11/2023', dataPagamento: null, valor: 120.00, pessoa: 'Top life +' },
-    { tipo : 'RECEITA', descricao: 'Venda do carro', dataVencimento: '19/11/2023', dataPagamento: null, valor: 65000.00, pessoa: 'João Ferreira' },
-    { tipo : 'DESPESA', descricao: 'Feira', dataVencimento: '19/11/2023', dataPagamento: null, valor: 750.00, pessoa: 'Mercado pague bem' },
-    { tipo : 'DESPESA', descricao: 'Seguro do carro', dataVencimento: '05/12/2023', dataPagamento: null, valor: 3200.00, pessoa: 'Seguradora Líder' },
-    { tipo : 'DESPESA', descricao: 'Barbearia', dataVencimento: '10/11/2023', dataPagamento: null, valor: 45.00, pessoa: 'Style man' },
-    { tipo : 'DESPESA', descricao: 'Posto de lavagem', dataVencimento: '18/11/2023', dataPagamento: null, valor: 50.00, pessoa: 'Lava-jato do marquinhos' }
+    { tipo : 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2023,10,21), dataPagamento: null, valor: 8000.00, pessoa: 'Drogaria Saúde' },
+    { tipo : 'RECEITA', descricao: 'Aluguel', dataVencimento: new Date(2023,11,5), dataPagamento: null, valor: 1200.00, pessoa: 'Fernando Pessoa' },
+    { tipo : 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2023,10,19), dataPagamento: null, valor: 6.50, pessoa: 'Padaria pão de mel' },
+    { tipo : 'DESPESA', descricao: 'Impostos', dataVencimento: new Date(2023,10,30), dataPagamento: null, valor: 2350.00, pessoa: 'Ministério da Fazenda' },
+    { tipo : 'DESPESA', descricao: 'Mensalidade da escola', dataVencimento: new Date(2023,11,5), dataPagamento: null, valor: 400.00, pessoa: 'Cantinho do saber' },
+    { tipo : 'DESPESA', descricao: 'Mensalidade da academia', dataVencimento: new Date(2023,10,30), dataPagamento: null, valor: 120.00, pessoa: 'Top life +' },
+    { tipo : 'RECEITA', descricao: 'Venda do carro', dataVencimento: new Date(2023,10,19), dataPagamento: null, valor: 65000.00, pessoa: 'João Ferreira' },
+    { tipo : 'DESPESA', descricao: 'Feira', dataVencimento: new Date(2023,10,19), dataPagamento: null, valor: 750.00, pessoa: 'Mercado pague bem' },
+    { tipo : 'DESPESA', descricao: 'Seguro do carro', dataVencimento: new Date(2023,11,5), dataPagamento: null, valor: 3200.00, pessoa: 'Seguradora Líder' },
+    { tipo : 'DESPESA', descricao: 'Barbearia', dataVencimento: new Date(2023,10,10), dataPagamento: null, valor: 45.00, pessoa: 'Style man' },
+    { tipo : 'DESPESA', descricao: 'Posto de lavagem', dataVencimento: new Date(2023,10,18), dataPagamento: null, valor: 50.00, pessoa: 'Lava-jato do marquinhos' }
   ];
 }
